9598a856a53798682fe3ad1ae91a32539fb5d3ba
DeeppParmar <deepparmar23082005@gmail.com>
2026-04-07 23:21:26 +0530

    fix

--- server/__init__.py
+++ server/__init__.py
@@ -1,2 +1,6 @@
-"""Server package for OpenEnv multi-mode deployment checks."""
+"""Server package exposing ASGI app for `uvicorn server:app`."""
+
+from server.app import app, main
+
+__all__ = ["app", "main"]
 
